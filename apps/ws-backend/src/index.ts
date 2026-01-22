fa7a3be8eac43e3296d1a7a1f6c538d596a862a9
Dev kumar <devkmandal0@gmail.com>
2026-01-23 00:48:26 +0530

    Add JWT configuration and update dependencies in ws-backend. Introduced config file for JWT_SECRET, modified tsconfig build info, and updated package.json to include jsonwebtoken and its types.

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,5 +1,6 @@
 import {WebSocketServer} from 'ws';
-
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { JWT_SECRET } from './config';
 
 const wss = new WebSocketServer({
     port:8080
@@ -15,8 +16,14 @@ wss.on('connection',(ws,req)=>{
     }
 
     const queryParams= new URLSearchParams(url.split('?')[1]);
-    const token = queryParams.get('token');
+    const token = queryParams.get('token') ?? "";
 
+    const decode = jwt.verify(token,JWT_SECRET);
+
+    if(!decode||!(decode as JwtPayload).userId){
+        ws.close();
+        return;
+    }
 
     ws.on('message',(message)=>{
         ws.send('pong');
